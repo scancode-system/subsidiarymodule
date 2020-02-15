28e60f5d3a697dade6c44c4fa75cd7214d757fd5
Your Name <you@example.com>
2020-02-15 06:49:44 -0300

    .

--- Entities/Subsidiary.php
+++ Entities/Subsidiary.php
@@ -3,8 +3,14 @@
 namespace Modules\Subsidiary\Entities;
 
 use Illuminate\Database\Eloquent\Model;
+use Modules\Product\Entities\Product;
 
 class Subsidiary extends Model
 {
-    protected $guarded = [];
+	protected $guarded = [];
+
+	public function products()
+	{
+		return $this->belongsToMany(Product::class, 'subsidiaries_products');
+	}
 }
--- Repositories/SubsidiaryRepository.php
+++ Repositories/SubsidiaryRepository.php
@@ -8,19 +8,32 @@ class SubsidiaryRepository
 {
 
 	// LOAD
-	public static function loadByName($name){
+	public static function load()
+	{
+		return Subsidiary::all();
+	}
+
+	public static function loadById($id)
+	{
+		return Subsidiary::find($id);
+	}
+
+	public static function loadByName($name)
+	{
 		return Subsidiary::where('name', $name)->first();
 	}
 
-	public static function loadToSelect($value, $description){
+	public static function loadToSelect($value, $description)
+	{
 		return Subsidiary::pluck($description, $value);
 	}
 
 
 	// SAVE
-	public static function store($data){
+	public static function store($data)
+	{
 		return Subsidiary::create($data);
 	}
- 
+
 
 }
